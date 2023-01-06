27b4ef1727ebf4569fe7dff5390f0c9dd1612fe3
= <ardayildirim05@icloud.com>
2023-01-06 18:52:59 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Cloud-Hosts-School-Project
---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+
+
+import * as React from 'react';
+import { Component } from 'react';
+
+interface AppProps {
+  
+}
+ 
+interface AppState {
+  
+}
+ 
+class App extends React.Component<AppProps, AppState> {
+  state = {}
+
+  render() { 
+    return (
+      <React.Fragment>
+        
+      </React.Fragment>
+    );
+  }
+}
+ 
+export default App;

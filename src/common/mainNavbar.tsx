2e1ea4f0547b4023b764622f36f4188e7616d159
Arda Yildirim <ardayildirim05@icloud.com>
2023-01-08 16:34:58 +0100

    Implement navigation sidebar

--- src/common/mainNavbar.tsx
+++ src/common/mainNavbar.tsx
@@ -2,6 +2,8 @@
 
 import * as React from 'react';
 import { Component } from 'react';
+import AppContext from '../AppContext';
+import NavigationSidebar from './navigationSidebar';
 
 interface MainNavbarProps {
     
@@ -12,15 +14,23 @@ interface MainNavbarState {
 }
  
 class MainNavbar extends React.Component<MainNavbarProps, MainNavbarState> {
+    static contextType = AppContext;
+    declare context: React.ContextType<typeof AppContext>;
+
     state = {}
+
     render() { 
+        const {onNavigationOpen} = this.context;
+
         return (
             <nav className="top-nav">
+                <NavigationSidebar />
+
                 <div>
                     <img className="icon-small" src="../public/icons/usa.png"/>
                 </div>
                 <h1 className="title capitalize">cloud hosts</h1>
-                <i className="top-nav__btn fa fa-bars"/>
+                <i onClick={onNavigationOpen} className="top-nav__btn fa fa-bars"/>
             </nav>
         );
     }

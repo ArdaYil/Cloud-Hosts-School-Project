d55541ae8475c8d4b60b7ba2eb367143fb3b6f79
= <ardayildirim05@icloud.com>
2023-03-09 19:55:02 +0100

    a

---
+++ src/reviewsPage.tsx
@@ -0,0 +1,21 @@
+
+
+import * as React from 'react';
+
+interface ReviewsPageProps {
+    
+}
+ 
+interface ReviewsPageState {
+    
+}
+ 
+class ReviewsPage extends React.Component<ReviewsPageProps, ReviewsPageState> {
+    state = {}
+
+    render() { 
+        return ();
+    }
+}
+ 
+export default ReviewsPage;
---
+++ src/services/httpService.ts
@@ -0,0 +1,9 @@
+
+
+import axios from "axios";
+
+const endpoint: string = "";
+
+export function get() {
+    return axios.get()
+}
